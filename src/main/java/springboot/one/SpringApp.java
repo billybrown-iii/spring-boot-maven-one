8cfdb691c969527b2cdbfdfc4d30835f8f9fa732
Billy <billybdev@gmail.com>
2022-11-23 16:33:56 -0800

    init

---
+++ src/main/java/springboot/one/SpringApp.java
@@ -0,0 +1,13 @@
+package springboot.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApp {
+
+    public static void main(String[] args) {
+        // System.out.println("Hello World!");
+        SpringApplication.run(SpringApp.class, args);
+    }
+}
